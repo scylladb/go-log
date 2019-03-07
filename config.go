07b98dfe9d60d8c5803e3e7f06649dba5ce3595f
Michał Matczuk <michal@scylladb.com>
2019-03-07 10:24:03 +0100

    config: use short keys and StringDurationEncoder for time

--- config.go
+++ config.go
@@ -67,10 +67,24 @@ func NewProduction(c Config) (Logger, error) {
 		zap.AddCallerSkip(2),
 	}
 
+	cfg := zapcore.EncoderConfig{
+		// Keys can be anything except the empty string.
+		TimeKey:        "T",
+		LevelKey:       "L",
+		NameKey:        "N",
+		CallerKey:      "C",
+		MessageKey:     "M",
+		StacktraceKey:  "S",
+		LineEnding:     zapcore.DefaultLineEnding,
+		EncodeLevel:    zapcore.CapitalLevelEncoder,
+		EncodeTime:     zapcore.ISO8601TimeEncoder,
+		EncodeDuration: zapcore.StringDurationEncoder,
+		EncodeCaller:   zapcore.ShortCallerEncoder,
+	}
+
 	var core zapcore.Core
 	switch c.Mode {
 	case StderrMode:
-		cfg := zap.NewProductionEncoderConfig()
 		core = zapcore.NewCore(
 			zapcore.NewJSONEncoder(cfg),
 			zapcore.Lock(os.Stderr),
@@ -83,7 +97,6 @@ func NewProduction(c Config) (Logger, error) {
 		}
 
 		// ignore level and time as they will be logged by syslog
-		cfg := zap.NewProductionEncoderConfig()
 		cfg.LevelKey = ""
 		cfg.TimeKey = ""
 
